3e26014e7e521f3a98e8821cf7a1dc55e85ab843
Keith Petersen <krp@keithrpetersen.com>
2022-11-29 14:37:36 -0600

    change lambda name to match repo name

--- infra/tf/lambda.tf
+++ infra/tf/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "python_custom" {
   depends_on = [
     null_resource.ecr_image
   ]
-  function_name = "lambda_python_custom"
+  function_name = "lambda-python-custom"
   role          = aws_iam_role.lambda_python_custom.arn
   package_type  = "Image"
   image_uri     = "${aws_ecr_repository.repo.repository_url}@${data.aws_ecr_image.lambda_image.image_digest}"
