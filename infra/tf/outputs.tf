800039a475e29ce950efbcb3be9c6b6a8a531bb2
Keith Petersen <krp@keithrpetersen.com>
2022-12-05 15:37:09 -0600

    remove ecr image outputs

--- infra/tf/outputs.tf
+++ infra/tf/outputs.tf
@@ -9,11 +9,3 @@ output "function_url" {
 output "lambda_image_uri" {
   value = aws_lambda_function.this.image_uri
 }
-
-output "image_digest" {
-  value = data.aws_ecr_image.lambda_image.image_digest
-}
-
-output "image_tags" {
-  value = data.aws_ecr_image.lambda_image.image_tags
-}
