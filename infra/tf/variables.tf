b5408b3bf896c5f6c702e451cb4e712ba1930b1e
Keith Petersen <krp@keithrpetersen.com>
2022-12-05 15:34:17 -0600

    terraform fmt

--- infra/tf/variables.tf
+++ infra/tf/variables.tf
@@ -1,22 +1,22 @@
 variable "region" {
   description = "AWS region"
-  type = string
-  default = "us-east-2"
+  type        = string
+  default     = "us-east-2"
 }
 
 variable "image_tag" {
   description = "the image tag for the docker image in ECR"
-  type = string
+  type        = string
 }
 
 variable "function_name" {
   description = "the name of the lambda function and its Cloudwatch log group"
-  type = string
-  default = "lambda-python-custom"
+  type        = string
+  default     = "lambda-python-custom"
 }
 
 variable "log_retention_days" {
   description = "the number days to keep Cloudwatch logs"
-  type = number
-  default = 60
+  type        = number
+  default     = 60
 }
