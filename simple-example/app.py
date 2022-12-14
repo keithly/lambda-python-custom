d7d6093fbdee107d76ca22edbf0b2cdcc7e4dc9d
Keith Petersen <krp@keithrpetersen.com>
2022-12-14 13:06:13 -0600

    [skip ci] add a simpler example using official Python image

---
+++ simple-example/app.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/html"
+        },
+        "body": f"<h1>Hello world from custom runtime Python {sys.version}</h1><p>{event}</p>"
+    }
