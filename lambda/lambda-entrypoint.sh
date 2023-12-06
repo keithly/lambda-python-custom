a7acadd2f1a165cf5d0a3a13ffaa9a297b8c371b
Keith Petersen <krp@keithrpetersen.com>
2023-12-06 14:43:21 -0600

    drop AL 2 and start from lambda/provided:al2023 don't build openssl gpg doesn't work, try sigstore cosign

--- lambda/lambda-entrypoint.sh
+++ lambda/lambda-entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 if [ $# -ne 1 ]; then
   echo "entrypoint requires the handler name to be the first argument" 1>&2
   exit 142
 fi
-export _HANDLER="$1"
 
-RUNTIME_ENTRYPOINT=/var/runtime/bootstrap
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-  exec /usr/local/bin/aws-lambda-rie $RUNTIME_ENTRYPOINT
+  exec /usr/local/bin/aws-lambda-rie /usr/local/bin/python -m awslambdaric "$@"
 else
-  exec $RUNTIME_ENTRYPOINT
+  exec /usr/local/bin/python -m awslambdaric "$@"
 fi
