3e13f821a4349b8e18cdbb5dc64f125dbb4a4490
Keith Petersen <krp@keithrpetersen.com>
2023-12-07 11:08:48 -0600

    give up on python download verifcation add missing dependencies move all custom code to /var/task

--- lambda/lambda-entrypoint.sh
+++ lambda/lambda-entrypoint.sh
@@ -6,7 +6,7 @@ if [ $# -ne 1 ]; then
 fi
 
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-  exec /usr/local/bin/aws-lambda-rie /usr/local/bin/python -m awslambdaric "$@"
+  exec /usr/local/bin/aws-lambda-rie /var/lang/bin/python -m awslambdaric --log-level "debug" "$@"
 else
-  exec /usr/local/bin/python -m awslambdaric "$@"
+  exec /var/lang/bin/python -m awslambdaric "$@"
 fi
